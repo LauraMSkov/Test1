5ec572ceee970707fd35149cb23b0dc539c6d5de
Laura Marie Balle Jensen <laje11@student.eadania.dk>
2026-02-09 12:02:53 +0100

    Med kanter

--- Test/Program.cs
+++ Test/Program.cs
@@ -21,6 +21,33 @@ namespace Test
             Console.WriteLine("Sammenligninger: " + comparisonQuick);
 
             Graph<string> graph = new Graph<string>();
+
+            //Tilføjer nodes
+            graph.AddNode("Entrance");
+            graph.AddNode("Carousel");
+            graph.AddNode("Mini Train");
+            graph.AddNode("Ice Cream");
+            graph.AddNode("Roller Coaster");
+            graph.AddNode("Haunted");
+            graph.AddNode("Water Ride");
+            graph.AddNode("Pirate Ship");
+            graph.AddNode("Climbing Tower");
+            graph.AddNode("Volcano Ride");
+
+            //Tilføjer kanter fra Indgang
+            graph.AddEdge("Entrance", "Carousel");
+            graph.AddEdge("Entrance", "Mini Train");
+            graph.AddEdge("Entrance", "Ice Cream");
+
+            //Tilføjer kanter fra Carousel
+            graph.AddEdge("Carousel", "Roller Coaster");
+            graph.AddEdge("Carousel", "Haunted House");
+
+            //Tilføjer kanter fra Roller Coaster
+            graph.AddEdge("Roller Coaster", "Climbing Tower");
+
+            //Tilføjer Kanter fra Climbing Tower
+            graph.AddEdge("Climbing Tower", "Volcano Ride");
         }
     }
 }
