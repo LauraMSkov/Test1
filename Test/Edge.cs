1d99e99366a9af8627926bd2475ffe87d975548c
Laura Marie Balle Jensen <laje11@student.eadania.dk>
2026-02-09 10:07:22 +0100

    Del 2

---
+++ Test/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Edge<T>
+    {
+        public Node<T> From { get; set; }
+        public Node<T> To { get; set; }
+
+        public Edge(Node<T> from, Node<T> to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+    }
+}
---
+++ Test/Graph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Graph<T>
+    {
+        public List<Node<T>> Nodes { get; set; } = new List<Node<T>>();
+
+        public void AddNode(T node)
+        {
+            Nodes.Add(new Node<T>(node));
+        }
+    }
+}
